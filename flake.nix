8c6f66ef67cbc79466089020251da98643e1d0bb
Lin Xianyi <iynaix@gmail.com>
2023-07-31 12:51:32 +0800

    Update dependencies

--- flake.nix
+++ flake.nix
@@ -1,34 +1,42 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/default";
     devenv.url = "github:cachix/devenv";
   };
 
-  outputs = { self, nixpkgs, devenv, ... } @ inputs:
-    let
-      systems = [ "x86_64-linux" ];
-      forAllSystems = f: builtins.listToAttrs (map (name: { inherit name; value = f name; }) systems);
-    in
-    {
-      devShells = forAllSystems
-        (system:
-          let
-            pkgs = import nixpkgs {
-              inherit system;
-            };
-          in
-          {
-            default = devenv.lib.mkShell {
-              inherit inputs pkgs;
-              modules = [
-                {
-                  # https://devenv.sh/reference/options/
-                  packages = with pkgs; [ yarn ];
+  outputs = {
+    self,
+    nixpkgs,
+    devenv,
+    systems,
+    ...
+  } @ inputs: let
+    forEachSystem = nixpkgs.lib.genAttrs (import systems);
+  in {
+    devShells =
+      forEachSystem
+      (system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        default = devenv.lib.mkShell {
+          inherit inputs pkgs;
+          modules = [
+            {
+              # https://devenv.sh/reference/options/
+              dotenv.disableHint = true;
 
-                  languages.javascript.enable = true;
-                }
-              ];
-            };
-          });
-    };
+              # scripts.yarn-reset.exec = ''
+              #   rm -rf node_modules && rm yarn.lock && rm -rf .next && yarn
+              # '';
+
+              packages = with pkgs; [yarn];
+
+              languages.javascript.enable = true;
+              languages.typescript.enable = true;
+            }
+          ];
+        };
+      });
+  };
 }
